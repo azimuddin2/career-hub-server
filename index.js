9a8013ddd3eb5e8f4d1dcba251eb477c60c93f0d
azimuddin2 <mohammadazimuddin274@gmail.com>
2024-05-10 09:23:12 +0600

    added database connection and data json file

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
 const express = require('express');
 const app = express();
+require('dotenv').config()
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// database connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.emptc9i.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+
+    }
+    finally { }
+}
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
